7c18d9b6eb6a193e928fb09ec9b9f06f82a415d9
PJ Glover <pjglover11@gmail.com>
2024-07-19 22:36:10 -0400

    Added cone to camera and remade prefab and made detection script
    Detection script works but have not made the alert enemies function yet

--- Office Space/Assets/Scripts/SecurityCameraController.cs
+++ Office Space/Assets/Scripts/SecurityCameraController.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class SecurityCameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float maxTime;
+
+    float totalTime;
+    bool playerSpotted = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerSpotted)
+        {
+            totalTime += Time.deltaTime;
+            if(totalTime > maxTime)
+            {
+                alertEnemies();
+            }
+        }
+        if (!playerSpotted)
+        {
+            totalTime--;//Reduces time in camera but not instantly so if you go out and right back in it compounds
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            
+            playerSpotted = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerSpotted = false;
         }
     }
 
