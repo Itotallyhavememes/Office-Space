cd8eebcd33fccc5413dc9d87dd0bd2f7f70a7fb0
SuperFernDev <154095668+SuperFernDev@users.noreply.github.com>
2024-07-23 19:01:37 -0400

    Fixed Item Pickup Script
    +Made shuriken pick up and call GetWeaponStats
    +Made hit effect on the hand smaller

--- Office Space/Assets/Scripts/ItemPickup.cs
+++ Office Space/Assets/Scripts/ItemPickup.cs
@@ -4,103 +4,85 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
-    //[SerializeField] enum ItemType { health, speedBoost, shuriken, rubberBand, weapon }
-    //[SerializeField] ItemType type;
-    //[SerializeField] float rotationSpeed;
-    //[SerializeField] int bobSpeed;
-    //[SerializeField] float bobHeight;
-    //[SerializeField] PowerUpEffect powerupEffect;
-    //[SerializeField] WeaponStats weapon;
+    [SerializeField] enum ItemType { health, speedBoost, rubberBand, weapon }
+    [SerializeField] ItemType type;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] int bobSpeed;
+    [SerializeField] float bobHeight;
+    [SerializeField] PowerUpEffect powerupEffect;
+    [SerializeField] WeaponStats weapon;
 
 
-    //Vector3 startPos;
+    Vector3 startPos;
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    startPos = transform.position;
-    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    Spin();
-    //    Bob();
-    //}
+    // Update is called once per frame
+    void Update()
+    {
+        Spin();
+        Bob();
+    }
 
-    //void Spin()
-    //{
-    //    transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f, Space.Self);
-    //}
+    void Spin()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f, Space.Self);
+    }
 
-    //void Bob()
-    //{
-    //    transform.position = new Vector3(startPos.x, startPos.y + (bobHeight * Mathf.Sin(Time.time * bobSpeed)), startPos.z);
-    //}
+    void Bob()
+    {
+        transform.position = new Vector3(startPos.x, startPos.y + (bobHeight * Mathf.Sin(Time.time * bobSpeed)), startPos.z);
+    }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        switch (type)
-    //        {
-    //            case ItemType.health:
-    //                {
-    //                    if (GameManager.instance.playerScript.HP < GameManager.instance.playerScript.GetStartHP())
-    //                    {
-    //                        Destroy(gameObject);
-    //                        powerupEffect.ApplyBuff();
-    //                    }
-    //                    break;
-    //                }
-    //            case ItemType.speedBoost:
-    //                {
-    //                    Destroy(gameObject);
-    //                    powerupEffect.ApplyBuff();
-    //                    break;
-    //                }
-    //            case ItemType.shuriken:
-    //                {
-    //                    if (GameManager.instance.playerScript.shurikenAmmo < GameManager.instance.playerScript.GetStartShurikenAmmo())
-    //                    {
-    //                        GameManager.instance.playerScript.shurikenAmmo += 5;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch (type)
+            {
+                case ItemType.health:
+                    {
+                        if (GameManager.instance.playerScript.HP < GameManager.instance.playerScript.GetStartHP())
+                        {
+                            Destroy(gameObject);
+                            powerupEffect.ApplyBuff();
+                        }
+                        break;
+                    }
+                case ItemType.speedBoost:
+                    {
+                        Destroy(gameObject);
+                        powerupEffect.ApplyBuff();
+                        break;
+                    }
+                case ItemType.rubberBand:
+                    {
+                        if (GameManager.instance.playerScript.GetComponent<ItemThrow>().rubberBallCount < GameManager.instance.playerScript.GetComponent<ItemThrow>().GetMaxBallCount())
+                        {
+                            GameManager.instance.playerScript.GetComponent<ItemThrow>().rubberBallCount++;
+                            GameManager.instance.playerScript.GetComponent<ItemThrow>().updateGrenadeUI();
+                            Destroy(gameObject);
+                        }
 
-    //                        if (GameManager.instance.playerScript.shurikenAmmo < GameManager.instance.playerScript.GetStartShurikenAmmo())
-    //                        {
-    //                            GameManager.instance.playerScript.shurikenAmmo = GameManager.instance.playerScript.GetStartShurikenAmmo();
-    //                        }
+                        break;
+                    }
+                case ItemType.weapon:
+                    {
+                        GameManager.instance.playerScript.GetWeaponStats(this.weapon);
+                        Destroy(gameObject);
 
-    //                        GameManager.instance.playerScript.UpdateAmmoUI();
-    //                        Destroy(gameObject);
-    //                    }
-    //                    break;
-    //                }
-    //            case ItemType.rubberBand:
-    //                {
-    //                    if (GameManager.instance.playerScript.GetComponent<ItemThrow>().rubberBallCount < GameManager.instance.playerScript.GetComponent<ItemThrow>().GetMaxBallCount())
-    //                    {
-    //                        GameManager.instance.playerScript.GetComponent<ItemThrow>().rubberBallCount++;
-    //                        GameManager.instance.playerScript.GetComponent<ItemThrow>().updateGrenadeUI();
-    //                        Destroy(gameObject);
-    //                    }
-
-    //                    break;
-    //                }
-    //            case ItemType.weapon:
-    //                {
-    //                    if (GameManager.instance.playerScript.GetComponent<ItemThrow>().rubberBallCount < GameManager.instance.playerScript.GetComponent<ItemThrow>().GetMaxBallCount())
-    //                    {
-                            
-    //                    }
-
-    //                    break;
-    //                }
-    //            default:
-    //                break;
-    //        }
+                        break;
+                    }
+                default:
+                    break;
+            }
 
 
-    //    }
+        }
 
-    //}
+    }
 }
