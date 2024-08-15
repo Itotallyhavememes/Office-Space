40cfa6aa827f9653c2c67c702a71bd06625568ce
John Coss Vargas <155984383+JohnCV4065@users.noreply.github.com>
2024-08-15 19:59:24 -0400

    For sounds apply and return
    If you make some changes in sounds you click apply to save the change but if you make some changes and click return those changes will not save instead they will go back to whatever it was saved before you made the change.

--- Office Space/Assets/Scripts/Volume.cs
+++ Office Space/Assets/Scripts/Volume.cs
@@ -10,25 +10,52 @@ public class Volume : MonoBehaviour
     [SerializeField] private Slider MusterSider;
     [SerializeField] private Slider musicSider;
     [SerializeField] private Slider SFXSider;
+
+     bool changeAudio;
+ 
     private void Start()
     {
-        SetMusterVolume();
-        SetMusicVolume();
-        SetFXSVolume();
+        changeAudio = false;
+       
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            Debug.Log("Load was call");
+            LoadVolume();
+        }
+        else
+        {
+            Debug.Log("Load was not call");
+            SetMusicVolume();
+            SetFXSVolume();
+            SetMusterVolume();
+        }
+
+      
+        SFXSider.onValueChanged.AddListener((v)=>{
+            changeAudio = false;
+        });
+
+        MusterSider.onValueChanged.AddListener((v) => {
+            changeAudio = false;
+        });
+        musicSider.onValueChanged.AddListener((v) => {
+            changeAudio = false;
+        });
     }
+   
+
 
     public void SetMusterVolume()
     {
         float volume = MusterSider.value;
         audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
-
-
+      
     }
     public void SetMusicVolume()
     {
         float volume = musicSider.value;
         audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
-
 
     }
     public void SetFXSVolume()
@@ -36,6 +63,43 @@ public class Volume : MonoBehaviour
         float volume = SFXSider.value;
         audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
 
+    }
 
+    private void LoadVolume()
+    {
+        SFXSider.value = PlayerPrefs.GetFloat("SFXVolume");
+        musicSider.value= PlayerPrefs.GetFloat("MusicVolume");
+        MusterSider.value = PlayerPrefs.GetFloat("MusterVolume");
+        SetMusterVolume();
+        SetFXSVolume();
+        SetMusicVolume();
+    }
+
+    public void ApplyButton()
+    {
+        Debug.Log("Apply was call");
+        SetMusicVolume();
+        SetFXSVolume();
+        SetMusterVolume();
+     
+       PlayerPrefs.SetFloat("SFXVolume",SFXSider.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicSider.value);
+        PlayerPrefs.SetFloat("MusterVolume", MusterSider.value);
+        changeAudio = true;
+    }
+    public void myReturn()
+    {
+        if(!changeAudio)
+        {
+            Debug.Log("return was call");
+            SFXSider.value = PlayerPrefs.GetFloat("SFXVolume");
+            musicSider.value = PlayerPrefs.GetFloat("MusicVolume");
+            MusterSider.value = PlayerPrefs.GetFloat("MusterVolume");
+            SetFXSVolume();
+            SetMusterVolume();
+            SetMusicVolume();
+        }
+      
+            GameManager.instance.ReturnFromSettings();
     }
 }
